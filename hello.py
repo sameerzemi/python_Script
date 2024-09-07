595b3cca94a80bc0044f422563c913b284dd9bca
sameerzemi <sameerzemi@gmail.com>
2024-09-07 14:08:48 +0530

    chumma

--- hello.py
+++ hello.py
@@ -1,14 +1,5 @@
 print( "Hello kutta....")
-<<<<<<< HEAD
-
-
 a = 5
 b = 6
 sum = a + b
 print(f"Sum of {a} + {b} = {sum})
-=======
-a= 10
-b= 20
-sum = a+b
-print (sum)
->>>>>>> 4d4ae09e72af08e0e004735393dfd90890fbf2a1
